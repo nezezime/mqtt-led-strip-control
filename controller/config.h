87697bb38ae47aedb420148ab83e4eb7e7e2a0ee
jurek <jkopit1@gmail.com>
2021-01-17 19:09:20 +0100

    add config file

---
+++ controller/config.h
@@ -0,0 +1,11 @@
+// enter configuration in this file
+
+const char *ssid = "";
+const char *password = "";
+
+const char *mqtt_server = "";
+const char *mqtt_port = "1833";
+const char *mqtt_password = ""; // not usable without SSL
+
+const char *mqtt_topic_upstream = "";
+const char *mqtt_topic_downstream = "";
