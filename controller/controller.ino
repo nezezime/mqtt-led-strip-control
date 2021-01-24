53195a98e2c212fec780e72e93bbd0c78c206e24
jurek <jkopit1@gmail.com>
2021-01-24 15:19:46 +0100

    mqtt change color

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,6 +1,8 @@
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+
 #include "config.h" // create a file with the required constants
 
 // GPIO pin mapping
@@ -15,7 +17,81 @@
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
+#define NO_CHANGE -1
+#define COLOR_VALID(x) (x >= 0 && x < 256)
+
+struct ColorChannel {
+  int r = 0;
+  int g = 0;
+  int b = 0;
+
+  ColorChannel() {}
+  
+  void set(int r_new, int g_new, int b_new) {
+    r = r_new;
+    g = g_new;
+    b = b_new;
+  }
+  
+  void reset() {
+    r = 0;
+    g = 0;
+    b = 0;
+  }
+
+} currentColors;
+
+
+void set_channels(int r, int g, int b, bool animate = false) {
+
+  int r_new = COLOR_VALID(r) ? r : currentColors.r;
+  int g_new = COLOR_VALID(g) ? g : currentColors.g;
+  int b_new = COLOR_VALID(b) ? b : currentColors.b;
+
+  Serial.print("set_channels: ");
+  Serial.print(r_new);
+  Serial.print(", ");
+  Serial.print(g_new);
+  Serial.print(", ");
+  Serial.print(b_new);
+  Serial.print(" ");
+  if (animate) {
+    Serial.println("true");
+  } else {
+    Serial.println("false");
+  }
+
+  if (animate) {
+
+  } else {
+
+  }
+
+  analogWrite(R1, r_new);
+  analogWrite(R2, g_new);
+  analogWrite(R3, b_new);
+
+  analogWrite(L1, r_new);
+  analogWrite(L2, g_new);
+  analogWrite(L3, b_new);
+
+  currentColors.set(r_new, g_new, b_new);
+}
+
+void all_channels_off() {
+  analogWrite(R1, 0);
+  analogWrite(R2, 0);
+  analogWrite(R3, 0);
+
+  analogWrite(L1, 0);
+  analogWrite(L2, 0);
+  analogWrite(L3, 0);
+
+  currentColors.reset();
+}
+
 void wifi_connect() {
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("\n\n");
   Serial.print("Connecting to SSID: ");
@@ -53,6 +129,8 @@ void mqtt_connect() {
 }
 
 char message_buff[256];
+StaticJsonDocument<256> jsonBuffer;
+
 void mqtt_callback(char *led_control, byte *payload, unsigned int length)
 {
   Serial.print("Message arrived [");
@@ -69,17 +147,47 @@ void mqtt_callback(char *led_control, byte *payload, unsigned int length)
 
   String msgString = String(message_buff);
   Serial.println(msgString);
-  // TODO parse the received message
-}
+  
+  DeserializationError err = deserializeJson(jsonBuffer, msgString);
+  if (err) {
+    Serial.print("mqtt_callback: deserializeJson failed with code ");
+    Serial.println(err.c_str());
+    return;
+  }
 
-void all_channels_off() {
-  analogWrite(R1, 0);
-  analogWrite(R2, 0);
-  analogWrite(R3, 0);
+  int msgRed = NO_CHANGE;
+  JsonVariant v = jsonBuffer["data"]["r"];
+  if (!v.isNull()) {
+    if (v >= 0 && v < 256) {
+      msgRed = v;
+    }
+  }
 
-  analogWrite(L1, 0);
-  analogWrite(L2, 0);
-  analogWrite(L3, 0);
+  int msgGreen = NO_CHANGE;
+  v = jsonBuffer["data"]["g"];
+  if (!v.isNull()) {
+    if (v >= 0 && v < 256) {
+      msgGreen = v;
+    }
+  }
+
+  int msgBlue = NO_CHANGE;
+  v = jsonBuffer["data"]["b"];
+  if (!v.isNull()) {
+    if (v >= 0 && v < 256) {
+      msgBlue = v;
+    }
+  }
+
+  Serial.print("received r: ");
+  Serial.print(msgRed);
+  Serial.print(" g: ");
+  Serial.print(msgGreen);
+  Serial.print(" b: ");
+  Serial.print(msgBlue);
+  Serial.println("");
+
+  set_channels(msgRed, msgGreen, msgBlue, false);
 }
 
 void setup() {
@@ -103,6 +211,7 @@ void setup() {
 
   wifi_connect();
   mqtt_connect();
+  all_channels_off();
 }
 
 void loop() {
@@ -112,13 +221,5 @@ void loop() {
   }
   client.loop();
 
-  analogWrite(R1, 10);
-  analogWrite(R2, 10);
-  analogWrite(R3, 10);
-
-  analogWrite(L1, 10);
-  analogWrite(L2, 10);
-  analogWrite(L3, 10);
-
-  delay(1000);
+  delay(2000);
 }
